a3a8de2a405ca59b88c69a29bcf21e0a3135e48f
hebedebe <babyyodaismine@gmail.com>
2025-01-17 10:37:36 +1100

    http user creation

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,7 +1,7 @@
 import sqlite3 as sql
 
 def init_database(cursor):
-    cursor.execute("CREATE TABLE IF NOT EXISTS posts (parent id, id text, owner_uuid text, owner_username text, date text," 
+    cursor.execute("CREATE TABLE IF NOT EXISTS posts (parent_id text, id text, owner_uuid text, owner_username text, date text," 
                 "rating integer, title text, contents text, image_id text, comments text, likes integer)") #store comments in json form
 
     cursor.execute("CREATE TABLE IF NOT EXISTS users (uuid text, username text, name text, bio text, following text,"
--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,63 @@ from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHan
 
 from init_db import init_database
 
+def verify_user(username: str, uuid: str): #verify user against uuid
+    cur.execute("select * FROM users WHERE uuid=?", (uuid,))
+    user = cur.fetchone()
+    print(user)
+
+def does_entry_exist(table, heading, contents):
+    cur.execute(f"SELECT EXISTS(SELECT 1 FROM {table} WHERE {heading}=\"{contents}\")")
+    row = cur.fetchone()
+    return row != (0,)
+
+def create_user(uuid, username, name=""):
+    if (does_entry_exist("users", "username", username) or
+        does_entry_exist("users", "uuid", uuid)):
+        print("Failed to create user (username or uuid already exists)")
+        return False
+
+    cur.execute(f'INSERT INTO users (uuid, username, name) VALUES ("{uuid}", "{username}", "{name}");')
+    conn.commit()
+    print("Created user.")
+    return True
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print(self.path)
+        print(self.request)
+        print(self.requestline)
+        print()
+        self.send_response(200)
+        self.end_headers()
+        create_user("test_username", "test_uuid", "test_name")
+        self.wfile.write(b"recieved.")
+
+    def do_POST(self):
+        args = self.path.split("/")[1:]
+        print(args)
+        match args[0]:
+            case "create_user":
+                try:
+                    username = args[1]
+                    uuid = args[2]
+                    name = args[3]
+                    user_created = create_user(username, uuid, name)
+                    if user_created:
+                        self.send_response(201)
+                        self.end_headers()
+                    else:
+                        self.send_response(500)
+                        self.end_headers()
+                except:
+                    self.send_response(400)
+                    self.end_headers()
+
+            case _:
+                self.send_response(501)
+                self.end_headers()
+
 conn = sql.connect("server.db")
 cur = conn.cursor()
 
@@ -12,34 +69,3 @@ init_database(cur)
 IP, PORT = "0.0.0.0", 65432
 httpd = HTTPServer((IP, PORT), SimpleHTTPRequestHandler)
 httpd.serve_forever()
-
-
-def verify_user(username: str, uuid: str): #verify user against uuid
-    cur.execute("select * FROM users WHERE uuid=?", (uuid,))
-    user = cur.fetchone()
-    print(user)
-
-def does_entry_exist(table, heading, contents):
-    cur.execute(f"SELECT EXISTS(SELECT 1 FROM ? WHERE ?=\"?\"", (table, heading, contents))
-    return cur.fetchone()
-
-def create_user(uuid, username, name=""):
-    if (does_entry_exist("users", "username", username) or
-        does_entry_exist("users", "uuid", uuid)):
-        print("Failed to create user (username or uuid already exists)")
-        return False
-
-    cur.execute("INSERT INTO users (?, ?, ?)", (uuid, username, name))
-    conn.commit()
-    print("Created user.")
-    return True
-
-
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        create_user("test_username", "test_uuid", "test_name")
-        self.wfile.write(b"recieved.")
-
-
