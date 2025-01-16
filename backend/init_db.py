7b8ae861dde3626fae1a02f11b9bc9dbac2a8de8
hebedebe <babyyodaismine@gmail.com>
2025-01-16 17:55:40 +1100

    basic user management and beginning of http server

---
+++ backend/init_db.py
@@ -0,0 +1,9 @@
+import sqlite3 as sql
+
+def init_database(cursor):
+    cursor.execute("CREATE TABLE IF NOT EXISTS posts (parent id, id text, owner_uuid text, owner_username text, date text," 
+                "rating integer, title text, contents text, image_id text, comments text, likes integer)") #store comments in json form
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (uuid text, username text, name text, bio text, following text,"
+                "followers text, post_ids text)") #all lists (followers, posts, etc) stored in json
+
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,45 @@
 import sqlite3 as sql
+import requests
+from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+
+from init_db import init_database
 
 conn = sql.connect("server.db")
 cur = conn.cursor()
+
+init_database(cur)
+
+IP, PORT = "0.0.0.0", 65432
+httpd = HTTPServer((IP, PORT), SimpleHTTPRequestHandler)
+httpd.serve_forever()
+
+
+def verify_user(username: str, uuid: str): #verify user against uuid
+    cur.execute("select * FROM users WHERE uuid=?", (uuid,))
+    user = cur.fetchone()
+    print(user)
+
+def does_entry_exist(table, heading, contents):
+    cur.execute(f"SELECT EXISTS(SELECT 1 FROM ? WHERE ?=\"?\"", (table, heading, contents))
+    return cur.fetchone()
+
+def create_user(uuid, username, name=""):
+    if (does_entry_exist("users", "username", username) or
+        does_entry_exist("users", "uuid", uuid)):
+        print("Failed to create user (username or uuid already exists)")
+        return False
+
+    cur.execute("INSERT INTO users (?, ?, ?)", (uuid, username, name))
+    conn.commit()
+    print("Created user.")
+    return True
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        create_user("test_username", "test_uuid", "test_name")
+        self.wfile.write(b"recieved.")
+
+
